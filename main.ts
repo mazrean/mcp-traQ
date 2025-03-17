b7f0f728047d7ff370e8f0521fc9a0d4f0b363a8
mazrean <shunsuke.w04@gmail.com>
2025-03-17 19:03:34 +0900

    bot tokenオプション追加

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+import { program, Option } from "npm:commander";
+
 import { run } from "./mcp/server.ts";
+
+program.name("mcp-traq").description("MCP server for traQ").version("0.1.0");
+
+program.addOption(
+  new Option("-t, --token <token>", "traQ bot token").env("TRAQ_BOT_TOKEN")
+);
+program.parse();
 
 await run();
 console.error("MCP server exited");
