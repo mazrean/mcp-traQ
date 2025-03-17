ceb5ca1c4b75e9f8a0ef101ced33b4dd3c4a8860
mazrean <shunsuke.w04@gmail.com>
2025-03-17 16:43:20 +0900

    mcp init

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,33 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { server } from "./main.ts";
+import { Client } from "npm:@modelcontextprotocol/sdk/client/index.js";
+import { InMemoryTransport } from "npm:@modelcontextprotocol/sdk/inMemory.js";
+import { expect } from "jsr:@std/expect";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const client = new Client(
+  {
+    name: "test client",
+    version: "1.0",
+  },
+  {
+    capabilities: {},
+  }
+);
+const [clientTransport, serverTransport] = InMemoryTransport.createLinkedPair();
+await Promise.all([
+  client.connect(clientTransport),
+  server.connect(serverTransport),
+]);
+
+Deno.test("getStringLength", async () => {
+  const result = await client.callTool({
+    name: "getStringLength",
+    arguments: {
+      input: "Hello, world!",
+    },
+  });
+
+  expect(result).toEqual({
+    content: [{ type: "text", text: "13" }],
+    isError: false,
+  });
 });
